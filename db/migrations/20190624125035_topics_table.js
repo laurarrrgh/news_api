7730fe17332aea7acc00ce5e74cbcbcec23a2ac6
Laura <laurajadefogarty@gmail.com>
2019-06-24 13:00:13 +0100

    creates topics table migration

---
+++ db/migrations/20190624125035_topics_table.js
@@ -0,0 +1,17 @@
+exports.up = function(knex, Promise) {
+  console.log("creating topics table...");
+  return knex.schema.createTable(
+    "topics",
+    topicsTable =>
+      topicsTable
+        .integer("slug")
+        .primary()
+        .unique(),
+    topicsTable.text("description")
+  );
+};
+
+exports.down = function(knex, Promise) {
+  console.log(" removing topics table...");
+  return knex.schema.dropTable("topics");
+};
