a35b21458eded60e881f754106cc2bfbc1fbd5fe
Laura <laurajadefogarty@gmail.com>
2019-06-25 14:17:28 +0100

    completed utils

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,25 @@
-exports.formatDate = list => {};
+exports.formatDate = list => {
+  const formattedDate = [...list];
+  formattedDate.map(ele => {
+    ele["created_at"] = new Date(ele["created_at"]);
+  });
+  return formattedDate;
+};
 
-exports.makeRefObj = list => {};
+exports.makeRefObj = list => {
+  return list.reduce((accumulator, article) => {
+    accumulator[article.title] = article.article_id;
+    return accumulator;
+  }, {});
+};
 
-exports.formatComments = (comments, articleRef) => {};
+exports.formatComments = (comments, articleRef) => {
+  let formattedComments = comments.map(comment => ({
+    article_id: articleRef[comment.belongs_to],
+    body: comment.body,
+    author: comment.created_by,
+    votes: comment.votes,
+    created_at: new Date(comment.created_at)
+  }));
+  return formattedComments;
+};
