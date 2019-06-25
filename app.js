e0e0d8bc72050c2bc79d4898d98da906dfcce831
Laura <laurajadefogarty@gmail.com>
2019-06-25 16:21:59 +0100

    app.js destructuring amends

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const { apiRouter } = require("./routers/apiRouter");
 
 app.use(express.json());
+
 app.use("/api", apiRouter);
 
 module.exports = { app };
