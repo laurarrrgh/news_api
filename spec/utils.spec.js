94a989c5bce57c959bb9945326a6dd0d24f3c433
Laura <laurajadefogarty@gmail.com>
2019-06-25 14:16:45 +0100

    completed utils testing

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,8 +1,113 @@
-const { expect } = require('chai');
-const { formatDate, makeRefObj, formatComments } = require('../db/utils/utils');
+const { expect } = require("chai");
+const { formatDate, makeRefObj, formatComments } = require("../db/utils/utils");
 
-describe('formatDate', () => {});
+describe("formatDate", () => {
+  it("converts time format for single object", () => {
+    const oldTime = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
 
-describe('makeRefObj', () => {});
+    const newTime = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "Thu Nov 15 2018 12:21:54 GMT+0000 (Greenwich Mean Time)",
+        votes: 100
+      }
+    ];
+    expect(formatDate(oldTime)).to.eql(newTime);
+  });
+  it("converts time format for an array of objects", () => {
+    const oldTime = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: 1416140514171
+      }
+    ];
+    const newTime = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: "Thu Nov 15 2018 12:21:54 GMT+0000 (Greenwich Mean Time)",
+        votes: 100
+      },
+      {
+        title: "Sony Vaio; or, The Laptop",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+        created_at: "Sun Nov 16 2014 12:21:54 GMT+0000 (Greenwich Mean Time)"
+      }
+    ];
+    expect(formatDate(oldTime)).to.eql(newTime);
+  });
+});
 
-describe('formatComments', () => {});
+describe("makeRefObj", () => {
+  it("return a lookup object for one record", () => {
+    const listArray = [{ article_id: 1, title: "A" }];
+    const refObj = { A: 1 };
+    expect(makeRefObj(listArray)).to.eql(refObj);
+  });
+  it("return a lookup object for multiple records", () => {
+    const listArray = [
+      { article_id: 1, title: "A" },
+      { article_id: 2, title: "B" },
+      { article_id: 3, title: "C" }
+    ];
+    const refObj = { A: 1, B: 2, C: 3 };
+    expect(makeRefObj(listArray)).to.eql(refObj);
+  });
+});
+
+describe("formatComments", () => {
+  it("returns a new comment obj", () => {
+    const reference = { "They're not exactly dogs, are they?": 9 };
+    const originalComment = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        belongs_to: "They're not exactly dogs, are they?",
+        created_by: "butter_bridge",
+        votes: 16,
+        created_at: 1511354163389
+      }
+    ];
+    const amendedComment = [
+      {
+        body:
+          "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        article_id: 9,
+        author: "butter_bridge",
+        votes: 16,
+        created_at: "Wed Nov 22 2017 12:36:03 GMT+0000 (Greenwich Mean Time)"
+      }
+    ];
+
+    expect(formatComments(originalComment, reference)).to.eql(amendedComment);
+  });
+});
