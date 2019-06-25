e209dba171c6f98239bf78274f5bee965690586d
Laura <laurajadefogarty@gmail.com>
2019-06-25 16:18:50 +0100

    passed GET/topics status request 200 test

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -5,9 +5,21 @@ const chaiSorted = require("chai-sorted");
 const { app } = require("../app");
 const request = require("supertest")(app);
 const connection = require("../connection");
+chai.use(chaiSorted);
 
-describe("/api", () => {
-  describe("GET /topics", () => {
-    it();
+describe("/", () => {
+  describe("/api", () => {
+    // beforeEach(() => {
+    //   return connection.seed.run();
+    // });
+    // after(() => {
+    //   return connection.destroy();
+    // });
+
+    describe("GET /topics", () => {
+      it("status:200 responds with an array of topics", () => {
+        return request.get("/api/topics").expect(200);
+      });
+    });
   });
 });
