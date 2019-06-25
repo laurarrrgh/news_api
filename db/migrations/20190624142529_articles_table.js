6cf4d06795361b4f48f69ead655b56656d127718
Laura <laurajadefogarty@gmail.com>
2019-06-25 14:18:33 +0100

    amends to articles migration

--- db/migrations/20190624142529_articles_table.js
+++ db/migrations/20190624142529_articles_table.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   console.log("creating articles table...");
-  return knex.schema.createTable("artcles", articlesTable => {
+  return knex.schema.createTable("articles", articlesTable => {
     articlesTable.increments("article_id").primary();
     articlesTable.string("title");
-    articlesTable.string("body");
+    articlesTable.text("body");
     articlesTable.integer("votes").defaultTo(0);
     articlesTable.string("topic").references("topics.slug");
     articlesTable.string("author").references("users.username");
