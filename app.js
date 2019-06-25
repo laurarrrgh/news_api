4d66598196055d29efe6e2aa55adb4541ee904eb
Laura <laurajadefogarty@gmail.com>
2019-06-25 16:59:06 +0100

    added handle 500 errors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const { apiRouter } = require("./routers/apiRouter");
+const { handles500errors } = require("./errors");
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
+app.use(handles500errors);
 
 module.exports = { app };
