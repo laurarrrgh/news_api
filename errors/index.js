062ca52345e486a74ca9df060cd4711d09906ec0
Laura <laurajadefogarty@gmail.com>
2019-06-26 13:45:10 +0100

    added 404 handling for GET user

--- errors/index.js
+++ errors/index.js
@@ -1,10 +1,19 @@
-exports.handles500errors = (err, req, res, next) => {
-  res.status(500).send({ msg: err.message });
-};
 exports.routeNotFound = (req, res) => {
-  res.status(404).send({ msg: "Page does not exist" });
+  res.status(404).send({ msg: "Page Not Found" });
 };
 
 exports.methodNotAllowed = (req, res) => {
   res.status(405).send({ msg: "Method Not Allowed" });
+};
+
+exports.handleCustomeErrors = (err, req, res, next) => {
+  if (err.status === "404") {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+
+exports.handles500errors = (err, req, res, next) => {
+  res.status(500).send({ msg: err.message });
 };
