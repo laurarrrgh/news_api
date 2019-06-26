01592d8721c22a7ba0b5d7e27201e05977d2e187
Laura <laurajadefogarty@gmail.com>
2019-06-26 12:23:20 +0100

    added error handling for 404 & 405

--- errors/index.js
+++ errors/index.js
@@ -1,3 +1,10 @@
 exports.handles500errors = (err, req, res, next) => {
   res.status(500).send({ msg: err.message });
 };
+exports.routeNotFound = (req, res) => {
+  res.status(404).send({ msg: "Page does not exist" });
+};
+
+exports.methodNotAllowed = (req, res) => {
+  res.status(405).send({ msg: "Method Not Allowed" });
+};
