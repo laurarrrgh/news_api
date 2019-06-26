78f196a1dcb6110387c2c56b449d959debc59ad4
Laura <laurajadefogarty@gmail.com>
2019-06-26 14:06:47 +0100

    created articleController

---
+++ controller/articleController.js
@@ -0,0 +1,10 @@
+const { fetchArticle } = require("../models/articleModel.js");
+
+exports.getArticle = (req, res, next) => {
+  // const { username } = req.params;
+  // fetchArticle(username)
+  //   .then(user => {
+  //     res.status(200).send({ user });
+  //   })
+  // .catch(next);
+};
