7fdfeef81ab60c7a8e10daaed2627d15596ee779
Laura <laurajadefogarty@gmail.com>
2019-06-26 13:46:35 +0100

    completed GET /users/:userid

---
+++ controller/userController.js
@@ -0,0 +1,10 @@
+const { fetchUser } = require("../models/userModel.js");
+
+exports.getUser = (req, res, next) => {
+  const { username } = req.params;
+  fetchUser(username)
+    .then(user => {
+      res.status(200).send({ user });
+    })
+    .catch(next);
+};
