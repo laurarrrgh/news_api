44731a7988e017457d169d357d181598b4bc3ec5
Laura <laurajadefogarty@gmail.com>
2019-06-26 14:12:28 +0100

    required in articleRouter

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const apiRouter = express.Router();
 const { topicRouter } = require("./topicRouter.js");
 const { userRouter } = require("./userRouter.js");
+const { articleRouter } = require("./articleRouter.js");
 
 apiRouter.use("/topics", topicRouter);
 apiRouter.use("/users", userRouter);
