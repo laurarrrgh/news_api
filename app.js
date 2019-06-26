8bc55049d8f0e77e09a94dc6d4d3d38c1d3987b9
Laura <laurajadefogarty@gmail.com>
2019-06-26 12:22:18 +0100

    added err handling routes

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const { apiRouter } = require("./routers/apiRouter");
-const { handles500errors } = require("./errors");
+const {
+  handles500errors,
+  routeNotFound,
+  methodNotAllowed
+} = require("./errors");
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
 app.use(handles500errors);
+app.use(routeNotFound);
+app.use(methodNotAllowed);
 
 module.exports = { app };
