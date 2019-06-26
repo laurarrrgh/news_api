e19669bd0e4a0f42ce832813c684853d7b7a953a
Laura <laurajadefogarty@gmail.com>
2019-06-26 13:48:17 +0100

    completed GET /users/:userid

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const apiRouter = express.Router();
 const { topicRouter } = require("./topicRouter.js");
+const { userRouter } = require("./userRouter.js");
 
 apiRouter.use("/topics", topicRouter);
+apiRouter.use("/users", userRouter);
 
 module.exports = { apiRouter };
