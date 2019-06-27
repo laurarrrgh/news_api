53e3796a2ea96e095515ea42678cdb132ccefb5f
Laura <laurajadefogarty@gmail.com>
2019-06-27 09:12:41 +0100

    amends to article controller

--- controller/articleController.js
+++ controller/articleController.js
@@ -1,10 +1,22 @@
-const { fetchArticle } = require("../models/articleModel.js");
+const { fetchArticle, updateArticle } = require("../models/articleModel.js");
 
 exports.getArticle = (req, res, next) => {
-  // const { username } = req.params;
-  // fetchArticle(username)
-  //   .then(user => {
-  //     res.status(200).send({ user });
-  //   })
-  // .catch(next);
+  const { article_id } = req.params;
+  fetchArticle(+article_id)
+    .then(article => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
+};
+
+exports.patchArticle = (req, res, next) => {
+  const { inc_votes } = req.body;
+  const { article_id } = req.params; // this isnt coming through
+  //console.log(req.params, "req.params - controller");
+  // console.log(article_id, "no {} controller");
+  // console.log({ article_id }, "article id in { } controller");
+  console.log(res);
+  updateArticle(inc_votes, article_id)
+    .then(([article]) => res.status(200).send(article))
+    .catch(next);
 };
