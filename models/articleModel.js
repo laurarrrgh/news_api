dc0d736524ebb5e8afd4def05a989ab7b06a2dc2
Laura <laurajadefogarty@gmail.com>
2019-06-27 09:16:29 +0100

    amends to article model

--- models/articleModel.js
+++ models/articleModel.js
@@ -1,16 +1,36 @@
 const { connection } = require("../connection.js");
 
-const fetchArticle = username => {
-  // return connection
-  //   .from("users")
-  //   .where("username", "=", username)
-  //   .returning("*")
-  //   .then(user => {
-  //     if (user.length === 0) {
-  //       return Promise.reject({ status: "404", msg: "Page Not Found" });
-  //     }
-  //     return user[0];
-  //   });
+const fetchArticle = articleid => {
+  return connection
+    .from("articles")
+    .where("articles.article_id", "=", articleid)
+    .select("articles.*")
+    .count({ comment_count: "comment_id" })
+    .leftJoin("comments", "comments.article_id", "articles.article_id")
+    .groupBy("articles.article_id")
+    .then(article => {
+      if (article.length === 0) {
+        return Promise.reject({ status: "404", msg: "Page Not Found" });
+      }
+      return article[0];
+    });
 };
 
-module.exports = { fetchArticle };
+const updateArticle = (article_id, inc_votes) => {
+  // console.log(inc_votes, "inc_votes - model");
+  // console.log(article_id, "id - controller");
+  return connection
+    .increment("votes", inc_votes)
+    .from("articles")
+    .where({ article_id })
+    .returning("*");
+
+  // .then(articles => {
+  //   if (articles.length === 0) {
+  //     return Promise.reject({ status: "404", msg: "Page Not Found" });
+  //   }
+  //   return article[0];
+  // });
+};
+
+module.exports = { fetchArticle, updateArticle };
