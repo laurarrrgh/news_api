361511f415d639ca0676e78bd359a0a81bffba68
Laura <laurajadefogarty@gmail.com>
2019-06-27 12:43:40 +0100

    reordered error handling blocks

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const app = express();
 const { apiRouter } = require("./routers/apiRouter");
 const {
   handle400errors,
-  handles500errors,
+  handleCustomeErrors,
   routeNotFound,
   methodNotAllowed,
-  handleCustomeErrors
+  handles500errors
 } = require("./errors");
 
 app.use(express.json());
 
 app.use("/api", apiRouter);
 
-app.use(routeNotFound);
-app.use(methodNotAllowed);
 app.use(handle400errors);
 app.use(handleCustomeErrors);
+app.use(routeNotFound);
+app.use(methodNotAllowed);
 app.use(handles500errors);
 
 module.exports = { app };
