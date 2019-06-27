03e8be604e05bb0167078505aa5afe40912039d1
Laura <laurajadefogarty@gmail.com>
2019-06-27 16:34:46 +0100

    route amends

--- routers/articleRouter.js
+++ routers/articleRouter.js
@@ -2,12 +2,13 @@ const express = require("express");
 const articleRouter = express.Router({ mergeParams: true });
 const { getArticle, patchArticle } = require("../controller/articleController");
 const { commentRouter } = require("./commentRouter.js");
+const { getCommentsByArticleID } = require("../controller/commentController");
 
 articleRouter
   .route("/:article_id")
   .get(getArticle)
   .patch(patchArticle);
 
-articleRouter.use("/:article_id/", commentRouter);
+articleRouter.route("/:article_id/comments").get(getCommentsByArticleID); // add .post here & patch/del on comments router
 
 module.exports = { articleRouter };
