013aa7783db448e74e2249a6a96d9436d1e4e87a
Laura <laurajadefogarty@gmail.com>
2019-06-27 12:42:16 +0100

    added article error handling

--- errors/index.js
+++ errors/index.js
@@ -5,20 +5,20 @@ exports.handle400errors = (err, req, res, next) => {
   else next(err);
 };
 
+exports.handleCustomeErrors = (err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+};
+
 exports.routeNotFound = (req, res) => {
   res.status(404).send({ msg: "Page Not Found" });
 };
 
 exports.methodNotAllowed = (req, res) => {
   res.status(405).send({ msg: "Method Not Allowed" });
-};
-
-exports.handleCustomeErrors = (err, req, res, next) => {
-  if (err.status === "404") {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    next(err);
-  }
 };
 
 exports.handles500errors = (err, req, res, next) => {
