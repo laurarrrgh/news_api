41b9411d3b52a53ca41d06064662bacf62b11300
Laura <laurajadefogarty@gmail.com>
2019-06-27 16:40:42 +0100

    completes GET /api/articles/:article_id/comments happy path

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,13 +1,36 @@
-const { createComment } = require("../models/commentModel.js");
+const { createComment, fetchComments } = require("../models/commentModel.js");
+
+exports.getCommentsByArticleID = (req, res, next) => {
+  const { article_id } = req.params;
+  fetchComments(+article_id)
+    .then(comments => {
+      res.status(200).send({ comments });
+    })
+    .catch(console.log);
+};
 
 exports.postComment = (req, res, next) => {
-  createComment(
-    req.params["article_id"],
-    req.body["username"],
-    req.body["body"]
-  )
-    .then(([comment]) => {
-      return res.status(201).send({ comment });
+  const { article_id } = req.params;
+  // console.log(req);
+  // console.log(article_id);
+
+  createComment(req.body)
+    .then(comment => {
+      res.status(201).send({ comment });
     })
-    .catch(next);
+    .catch(console.log);
 };
+// const { article_id } = req.params;
+// console.log(article_id);
+// const { article_id } = req.params["article_id"];
+// const { author } = req.body["username"];
+// const { body } = req.body["body"];
+// console.log( article_id );
+// console.log(author);
+// console.log(body);
+// createComment(article_id, author, body)
+//   .then(comment => {
+//     return res.status(201).send({ comment });
+//   })
+//   .catch(next);
+// };
