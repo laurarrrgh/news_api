e92dd6f8f9625f48399045b2eaf8ed4e27d60e50
Laura <laurajadefogarty@gmail.com>
2019-06-27 16:35:04 +0100

    route amends

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,8 +1,27 @@
 const { connection } = require("../connection.js");
-const createComment = (article_id, author, body) => {
-  return connection("comments")
-    .insert({ article_id, author, body })
-    .returning("*");
+
+const fetchComments = articleid => {
+  return connection
+    .from("articles")
+    .join("comments", "comments.article_id", "articles.article_id")
+    .where("comments.article_id", "=", articleid)
+    .select("comment_id", "votes", "created_at", "author", "body");
+  // .groupBy("comments.comment_id");
+  // .then(comments => {
+  //   if (article.length === 0) {
+  //     return Promise.reject({ status: "404", msg: "Page Not Found" });
+  //   }
+  //   return comments;
+  // });
 };
 
-module.exports = { createComment };
+// onhold
+const createComment = () => {
+  return connection
+    .insert({ article_id, author, body })
+    .into("comments")
+    .returning("*")
+    .then(comments => comments[0]);
+};
+
+module.exports = { createComment, fetchComments };
