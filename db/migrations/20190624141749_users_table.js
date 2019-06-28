2b12865ef5d2eb54191c1e7caffb8c8f45926732
Laura <laurajadefogarty@gmail.com>
2019-06-28 10:37:46 +0100

    removed console logs

--- db/migrations/20190624141749_users_table.js
+++ db/migrations/20190624141749_users_table.js
@@ -1,5 +1,4 @@
 exports.up = function(knex, Promise) {
-  // console.log("creating users table...");
   return knex.schema.createTable("users", usersTable => {
     usersTable
       .string("username")
@@ -12,6 +11,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  //  console.log("removing articles table...");
   return knex.schema.dropTable("users");
 };
