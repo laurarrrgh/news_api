1270f010fc8d3443fac87d020270da6b862481ad
Laura <laurajadefogarty@gmail.com>
2019-07-11 20:32:20 +0100

    completed all commend end points

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,8 +1,15 @@
-const { createComment, fetchComments } = require("../models/commentModel.js");
+const {
+  createComment,
+  fetchComments,
+  updateComment,
+  removeComment
+} = require("../models/commentModel.js");
 
 exports.getCommentsByArticleID = (req, res, next) => {
   const { article_id } = req.params;
-  fetchComments(+article_id)
+  const { sort_by, order } = req.query;
+
+  fetchComments({ article_id, sort_by, order })
     .then(comments => {
       res.status(200).send({ comments });
     })
@@ -11,26 +18,42 @@ exports.getCommentsByArticleID = (req, res, next) => {
 
 exports.postComment = (req, res, next) => {
   const { article_id } = req.params;
-  // console.log(req);
-  // console.log(article_id);
+  const { username, body } = req.body;
 
-  createComment(req.body)
-    .then(comment => {
+  createComment(article_id, username, body)
+    .then(([comment]) => {
       res.status(201).send({ comment });
     })
-    .catch(console.log);
+    .catch(next);
 };
-// const { article_id } = req.params;
-// console.log(article_id);
-// const { article_id } = req.params["article_id"];
-// const { author } = req.body["username"];
-// const { body } = req.body["body"];
-// console.log( article_id );
-// console.log(author);
-// console.log(body);
-// createComment(article_id, author, body)
-//   .then(comment => {
-//     return res.status(201).send({ comment });
-//   })
-//   .catch(next);
-// };
+
+exports.patchComment = (req, res, next) => {
+  const { inc_votes } = req.body;
+  const { comment_id } = req.params;
+
+  updateComment(comment_id, inc_votes)
+    .then(comment => {
+      if (comment.length === 0) {
+        return Promise.reject({
+          status: 404,
+          message: "Page not found"
+        });
+      }
+      res.status(200).send({ comment: comment[0] });
+    })
+    .catch(next);
+};
+
+exports.deleteComment = (req, res, next) => {
+  const { comment_id } = req.params;
+
+  removeComment(comment_id)
+    .then(deletionCount => {
+      if (deletionCount === 0) {
+        return Promise.reject({ status: "404", msg: "Page Not Found" });
+      } else {
+        res.status(204).send();
+      }
+    })
+    .catch(next);
+};
