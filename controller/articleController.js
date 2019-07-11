9e9ba171d16fbaf5d6eadb48c99c67122c6a29b0
Laura <laurajadefogarty@gmail.com>
2019-07-11 20:30:41 +0100

    completed getllArticles queries

--- controller/articleController.js
+++ controller/articleController.js
@@ -5,7 +5,7 @@ const {
 } = require("../models/articleModel.js");
 
 exports.getArticle = (req, res, next) => {
-  const { article_id } = req.params;
+  const { article_id, sort_by, order_by } = req.params;
   fetchArticle(+article_id)
     .then(article => {
       res.status(200).send({ article });
@@ -25,18 +25,11 @@ exports.patchArticle = (req, res, next) => {
 };
 
 exports.getAllArticles = (req, res, next) => {
-  fetchAllArticles()
+  const { sort_by, order_by } = req.query;
+  console.log(req.params);
+  fetchAllArticles(req.query)
     .then(articles => {
       res.status(200).send(articles);
     })
-    .catch(console.log);
+    .catch(next);
 };
-
-// custom err?
-// promise.reject
-// .then(article => {
-//   if ({article_id} === 0 || ) {
-//     return Promise.reject({ status: "400", msg: "Bad Request" });
-//   }
-//   return article[0];
-// });
